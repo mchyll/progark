6a430a3436a2de192c12701edb57fba75be5f0b5
Magnus Conrad Hyll <magnus@hyll.no>
2020-02-09 22:51:11 +0100

    Comments, line endings

--- exercise2/pong/core/src/no/mchyll/progark/exercise2/pong/Game.java
+++ exercise2/pong/core/src/no/mchyll/progark/exercise2/pong/Game.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * Game implements the Singleton pattern.
+ */
 public class Game extends ApplicationAdapter {
 	private int width;
 	private int height;
--- exercise2/pong/core/src/no/mchyll/progark/exercise2/pong/State.java
+++ exercise2/pong/core/src/no/mchyll/progark/exercise2/pong/State.java
@@ -2,7 +2,11 @@ package no.mchyll.progark.exercise2.pong;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-abstract class State {
+/**
+ * Part of the State design pattern.
+ * Abstract class defining the common interface for the different game states.
+ */
+public abstract class State {
     public abstract void update(float deltaTime);
 
     public abstract void render(SpriteBatch spriteBatch);
